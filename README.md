4ab8386fb593a768a6da6bb08221e81cfb1fdc28
Mikhail Vospennikov <m.vospennikov@gmail.com>
2022-04-25 08:17:39 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LayoutAnchorKit
+
+A description of this package.
---
+++ Sources/LayoutAnchorKit/LayoutAnchorKit.swift
@@ -0,0 +1,6 @@
+public struct LayoutAnchorKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
